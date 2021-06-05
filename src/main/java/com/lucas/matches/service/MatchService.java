c91f653b4825605ff3e124724e57e394ebc58c6c
lasiqueira <lucas.asiq@gmail.com>
2021-06-05 23:36:40 +0100

    Add service and tests

---
+++ src/main/java/com/lucas/matches/service/MatchService.java
@@ -0,0 +1,57 @@
+package com.lucas.matches.service;
+
+import com.lucas.matches.infrastructure.document.MatchDocument;
+import com.lucas.matches.infrastructure.repository.MatchRepository;
+import com.lucas.matches.service.converter.MatchConverter;
+import com.lucas.matches.service.domain.License;
+import com.lucas.matches.service.domain.LicenseType;
+import com.lucas.matches.service.domain.Match;
+import com.lucas.matches.service.util.LicenseParser;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+public class MatchService {
+    private final LicenseParser licenseParser;
+    private final MatchRepository matchRepository;
+    private final MatchConverter matchConverter;
+
+    public MatchService(LicenseParser licenseParser, MatchRepository matchRepository, MatchConverter matchConverter) {
+        this.licenseParser = licenseParser;
+        this.matchRepository = matchRepository;
+        this.matchConverter = matchConverter;
+    }
+
+    public List<Match> getMatches(List<String> licenseStringList) {
+        List<License> licenses = licenseParser.parse(licenseStringList);
+        List<License> tournamentLicenses = licenses.stream().filter(license -> license.getLicenseType().equals(LicenseType.TOURNAMENT)).collect(Collectors.toList());
+        List<License> matchLicenses = licenses.stream().filter(license -> license.getLicenseType().equals(LicenseType.MATCH)).collect(Collectors.toList());
+
+        return matchConverter.convert(Stream.concat(
+                getMatchesByTournamentId(tournamentLicenses).stream(),
+                getMatchesByMatchId(matchLicenses).stream()).collect(Collectors.toList()));
+    }
+
+    private List<MatchDocument> getMatchesByTournamentId(List<License> tournamentLicenses) {
+        List<MatchDocument> matchDocuments = new ArrayList<>();
+        if (tournamentLicenses.size() > 0) {
+            List<String> ids = tournamentLicenses.stream().map(License::getId).collect(Collectors.toList());
+            matchDocuments = matchRepository.findByTournamentIdIn(ids);
+        }
+        return matchDocuments;
+    }
+
+    private List<MatchDocument> getMatchesByMatchId(List<License> matchLicenses) {
+        List<MatchDocument> matchDocuments = new ArrayList<>();
+        if (matchLicenses.size() > 0) {
+            List<String> ids = matchLicenses.stream().map(License::getId).collect(Collectors.toList());
+            matchDocuments = matchRepository.findByMatchIdIn(ids);
+        }
+        return matchDocuments;
+    }
+
+}
