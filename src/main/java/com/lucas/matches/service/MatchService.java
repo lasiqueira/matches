9ebafbb3d7692990ab6418cadec7c183d6110ee7
lasiqueira <lucas.asiq@gmail.com>
2021-06-06 20:19:08 +0100

    Add logging

--- src/main/java/com/lucas/matches/service/MatchService.java
+++ src/main/java/com/lucas/matches/service/MatchService.java
@@ -7,6 +7,8 @@ import com.lucas.matches.service.domain.License;
 import com.lucas.matches.service.domain.LicenseType;
 import com.lucas.matches.service.domain.Match;
 import com.lucas.matches.service.util.LicenseParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -19,18 +21,24 @@ import java.util.stream.Stream;
 @Service
 @CacheConfig(cacheNames = {"match"})
 public class MatchService {
+    private final Logger logger;
     private final LicenseParser licenseParser;
     private final MatchRepository matchRepository;
     private final MatchConverter matchConverter;
 
     public MatchService(LicenseParser licenseParser, MatchRepository matchRepository, MatchConverter matchConverter) {
+        this.logger = LoggerFactory.getLogger(MatchService.class);
         this.licenseParser = licenseParser;
         this.matchRepository = matchRepository;
         this.matchConverter = matchConverter;
     }
     @Cacheable(key = "#licenseStringList")
     public List<Match> getMatches(List<String> licenseStringList) {
+        logger.info("Parsing Licenses...");
+        logger.debug(licenseStringList.toString());
         List<License> licenses = licenseParser.parse(licenseStringList);
+
+        logger.info("Fetching matches...");
         List<License> tournamentLicenses = licenses.stream().filter(license -> license.getLicenseType().equals(LicenseType.TOURNAMENT)).collect(Collectors.toList());
         List<License> matchLicenses = licenses.stream().filter(license -> license.getLicenseType().equals(LicenseType.MATCH)).collect(Collectors.toList());
 
@@ -42,6 +50,7 @@ public class MatchService {
     private List<MatchDocument> getMatchesByTournamentId(List<License> tournamentLicenses) {
         List<MatchDocument> matchDocuments = new ArrayList<>();
         if (tournamentLicenses.size() > 0) {
+            logger.info("Fetching Matches by Tournament ID...");
             List<String> ids = tournamentLicenses.stream().map(License::getId).collect(Collectors.toList());
             matchDocuments = matchRepository.findByTournamentIdIn(ids);
         }
@@ -51,6 +60,7 @@ public class MatchService {
     private List<MatchDocument> getMatchesByMatchId(List<License> matchLicenses) {
         List<MatchDocument> matchDocuments = new ArrayList<>();
         if (matchLicenses.size() > 0) {
+            logger.info("Fetching Matches by Match ID...");
             List<String> ids = matchLicenses.stream().map(License::getId).collect(Collectors.toList());
             matchDocuments = matchRepository.findByMatchIdIn(ids);
         }
--- src/main/java/com/lucas/matches/service/util/LicenseParser.java
+++ src/main/java/com/lucas/matches/service/util/LicenseParser.java
@@ -3,6 +3,8 @@ package com.lucas.matches.service.util;
 import com.lucas.matches.service.exception.InvalidLicenseException;
 import com.lucas.matches.service.domain.License;
 import com.lucas.matches.service.domain.LicenseType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -10,6 +12,12 @@ import java.util.List;
 
 @Component
 public class LicenseParser {
+    private final Logger logger;
+
+    public LicenseParser() {
+        this.logger = LoggerFactory.getLogger(LicenseParser.class);
+    }
+
     public List<License> parse(List<String> licenseStringList){
         List<License> licenses = new ArrayList<>();
         licenseStringList.forEach(licenseString -> licenses.add(parse(licenseString)));
@@ -19,11 +27,13 @@ public class LicenseParser {
         String[] licenseArray = licenseString.split("-");
 
        if(licenseArray.length < 2){
+           logger.error("Invalid license: " + licenseString);
            throw new InvalidLicenseException("Invalid license.");
        }
         License license = new License(LicenseType.value(licenseArray[0]), licenseArray[1]);
         if(license.getLicenseType() == null || license.getId() == null || license.getId().isEmpty()){
-           throw new InvalidLicenseException("Invalid license.");
+            logger.error("Invalid license: " + licenseString);
+            throw new InvalidLicenseException("Invalid license.");
         }
         return license;
     }
