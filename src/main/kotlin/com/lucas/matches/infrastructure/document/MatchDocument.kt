962827552fff2555926bcd3a89399ce372604a1e
lasiqueira <lucas.asiq@gmail.com>
2022-07-31 22:15:20 +0100

    Transform MatchDocument into data class

--- src/main/kotlin/com/lucas/matches/infrastructure/document/MatchDocument.kt
+++ src/main/kotlin/com/lucas/matches/infrastructure/document/MatchDocument.kt
@@ -3,38 +3,12 @@ package com.lucas.matches.infrastructure.document
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.MongoId
 import java.time.LocalDateTime
-import java.util.*
 
 @Document(collection = "matches")
-class MatchDocument(@MongoId var id: String?,
+data class MatchDocument(@MongoId var id: String?,
                     var tournamentId: String,
                     var matchId: String,
                     var playerA: String,
                     var playerB: String,
                     var startDate: LocalDateTime,
-                    var summaryType: SummaryTypeDocument?) {
-
-
-    override fun equals(o: Any?): Boolean {
-        if (this === o) return true
-        if (o == null || javaClass != o.javaClass) return false
-        val that = o as MatchDocument
-        return id == that.id && tournamentId == that.tournamentId && matchId == that.matchId && playerA == that.playerA && playerB == that.playerB && startDate == that.startDate && summaryType == that.summaryType
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hash(id, tournamentId, matchId, playerA, playerB, startDate, summaryType)
-    }
-
-    override fun toString(): String {
-        return "MatchDocument{" +
-                "id='" + id + '\'' +
-                ", tournamentId='" + tournamentId + '\'' +
-                ", matchId='" + matchId + '\'' +
-                ", playerA='" + playerA + '\'' +
-                ", playerB='" + playerB + '\'' +
-                ", startDate=" + startDate +
-                ", summaryType=" + summaryType +
-                '}'
-    }
-}
+                    var summaryType: SummaryTypeDocument?)
